de984b50fbad61b3ec033ba43567e41139288978
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-10-12 04:59:58 -0300

    Header

---
+++ app/src/main/java/com/example/grupo_6/Header.kt
@@ -0,0 +1,46 @@
+package com.example.grupo_6
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun Header(title: String) {
+    Row(
+        modifier = Modifier
+            .width(412.dp)
+            .height(64.dp)
+            .background(Color(0xFFFCFCFC))
+            .padding(start = 2.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = "Navigation Icon",
+            tint = Color.Black
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = title,
+            fontSize = 20.sp,
+            color = Color.Black
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    Header(title = "Preview Title")
+}
